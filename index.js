f0a9ea6e0f3dc3430bb4ec2916be74c5f107cb12
rahul-sirpi <rahul@sirpi.io>
2025-05-19 22:12:13 +0530

    Update server to use environment variable for port and modify response message; add 'env' dependency to package.json and package-lock.json

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello World from Render!");
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on port ${port}`);
 });
