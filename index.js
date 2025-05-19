245cdf19f046b2c0ba8d985002b236c2a40e5653
rahul-sirpi <rahul@sirpi.io>
2025-05-19 22:26:01 +0530

    Update home route response message to reflect Employee APP

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World from Render!");
+  res.send("Home Route for Employee APP!");
 });
 
 app.listen(port, () => {
