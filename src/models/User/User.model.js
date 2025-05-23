70b9b21f692e510d8f38d558bb0f7a0f933b975b
rahul-sirpi <rahul@sirpi.io>
2025-05-23 22:27:15 +0530

    Add password reset functionality with OTP verification; implement request and reset password controllers, and update auth routes accordingly.

--- src/models/User/User.model.js
+++ src/models/User/User.model.js
@@ -1,5 +1,4 @@
 import mongoose from "mongoose";
-import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema({
   fname: {
@@ -45,18 +44,7 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-// 🔐 Pre-save middleware to hash password if modified
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
 
-  try {
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
-  } catch (err) {
-    next(err);
-  }
-});
 
 const User = mongoose.model("User", userSchema);
 export default User;
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -2,10 +2,14 @@ import express from "express";
 import { initiateSignup } from "../controllers/auth/initiateSignup.js";
 import { verifyOtpAndCreateUser } from "../controllers/auth/verifyOtpAndCreateUser.js";
 import { loginUser } from "../controllers/auth/login.js";
+import { requestPasswordReset } from "../controllers/auth/requestPasswordReset.js";
+import { verifyOtpAndResetPassword } from "../controllers/auth/verifyOtpAndResetPassword.js";
 
 const router = express.Router();
 router.post("/signup/send-otp", initiateSignup);
 router.post("/signup/verify-otp", verifyOtpAndCreateUser);
 router.post("/login", loginUser)
+router.post("/request-password-reset", requestPasswordReset);
+router.post("/reset-password", verifyOtpAndResetPassword);
 
 export default router;
